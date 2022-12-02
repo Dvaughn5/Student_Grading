01d014290a4a74a83ae40e0ac669e5a5ded12cb2
Dvaughn5 <vaughndorian@gmail.com>
2022-12-02 17:24:43 -0500

    Creating .ps1
    - This powershell script creates a folder on the user's C: drive, and installs the student grading executable from github. In addition, the script creates a desktop shortcut using Windows Script Host.

---
+++ PowerScript.ps1
@@ -0,0 +1,22 @@
+# Creating a path for the files in the C: drive of the user
+New-Item -Path "C:\" -Name "StuGrading" -ItemType "directory"
+
+# Obtaining the executable from my github, and setting a path for the download
+$url = "https://github.com/Dvaughn5/Student_Grading/blob/master/StudentGrading" # Download from here
+$path = "C:\StuGrading\StudentGrading.exe" # Store here 
+
+# Writing the request to download the executable 
+Invoke-WebRequest -Uri $url -OutFile $path
+
+# Creating the desktop shortcut 
+$ShortcutPath = "C:\Users\Desktop\Grading.lnk"
+
+# Creating Windows Script Object
+$WScript = New-Object -ComObject ("WScript.Shell")
+
+# Creating the actual shortcut
+$shortcut = $WScript.CreateShortcut($ShortcutPath)
+
+# Adding target path and saving
+$shortcut.TargetPath = $path
+$shortcut.Save()
