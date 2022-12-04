d06b6c7e22241073b274225419abaf734a9e61d1
Dvaughn5 <vaughndorian@gmail.com>
2022-12-04 13:36:36 -0500

    Changed exe to py
    - The executable is too large on GitHub, so I changed the executable to a python script so it can be retrieved. This means the program has to be manually run, but had it been a .exe everything would work.

--- PowerScript.ps1
+++ PowerScript.ps1
@@ -3,19 +3,25 @@ New-Item -Path "C:\" -Name "StuGrading" -ItemType "directory"
 
 # Obtaining the executable from my github, and setting a path for the download
 $url = "https://raw.githubusercontent.com/Dvaughn5/Student_Grading/master/App.py" # Download from here
-$path = "C:\StuGrading\StudentGrading.exe" # Store here 
+$path = "C:\StuGrading\StudentGrading.py" # Store here which also acts as a target for the desktop shortcut
 
+$icoUrl = "https://github.com/Dvaughn5/Student_Grading/blob/master/icons8-python-100.png"
+$icoPath = "C:\StuGrading\icon.ico"
 # Writing the request to download the executable 
 Invoke-WebRequest -Uri $url -OutFile $path
+Invoke-WebRequest -Uri $icoUrl -OutFile $icoPath
 
 # Creating the desktop shortcut 
-$ShortcutPath = "C:\Users\Desktop\Grading.lnk"
+$ShortcutPath = "C:\Public\Desktop\Grading.lnk"
 
 # Creating Windows Script Host Object
-$WScript = New-Object -ComObject ("WScript.Shell")
+$WScript = New-Object -ComObject WScript.Shell
 
 # Creating the actual shortcut
 $shortcut = $WScript.CreateShortcut($ShortcutPath)
+
+# Icon creation
+$shortcut.IconLocation = "C:\StuGrading\icon.ico"
 
 # Adding target path and saving
 $shortcut.TargetPath = $path
